3cf3d49a004049cd87cfee5102aacffca8423908
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2022-01-28 21:03:40 +0530

    Time: 3266 ms (5.14%), Space: 156.4 MB (5.14%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} arr
+ * @return {number[][]}
+ */
+var minimumAbsDifference = function(arr) {
+    let temp, min = Infinity, paths = {}
+  arr = arr.sort((a,b)=>{
+    if(a>b){
+      return 1
+    }
+    else if(a<b){
+      return -1
+    }
+    return 0
+  })
+  for(let i=0;i<arr.length-1;i++){
+      temp = arr[i]-arr[i+1]
+      temp = Math.abs(temp)
+      min = Math.min(temp,min)
+      if(paths[temp]){
+          paths[temp]=[...paths[min],[arr[i],arr[i+1]]]
+          
+      }
+      else{
+          paths[temp]=[[arr[i],arr[i+1]]]
+      }
+  }
+  return paths[min]
+};
