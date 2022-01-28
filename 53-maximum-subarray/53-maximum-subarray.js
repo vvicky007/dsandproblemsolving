35026fc23fafd6451a8c26b0631743411037c016
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2022-01-28 20:38:23 +0530

    Time: 158 ms (19.69%), Space: 50 MB (10.28%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let sum = 0;
+  let maxSum = -Infinity;
+  
+  if(nums.length === 0) return 0;
+  if(nums.length === 1) return nums[0]
+  
+  for(let i = 0;i<nums.length;i++){
+      sum+=nums[i];
+      maxSum = Math.max(maxSum,sum);
+      if(sum < 0) sum = 0;
+  }
+  return maxSum;  
+};
