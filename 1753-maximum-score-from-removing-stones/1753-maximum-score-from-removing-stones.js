3b87ed5309dc4244b5a71fe21643d8c268f8b248
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2022-01-30 20:37:36 +0530

    Time: 986 ms (6.06%), Space: 47.9 MB (6.06%) - LeetHub

---
+++ 1753-maximum-score-from-removing-stones/1753-maximum-score-from-removing-stones.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var maximumScore = function(a, b, c) {
+        let count = 0
+    let arr = [a,b,c]
+    const check = (arr)=>{
+      arr = arr.sort()
+      if(arr[1]===0&&arr[0]===0){
+        return true
+      }
+    }
+    while(!check(arr)){
+      arr = arr.sort((a,b)=>a-b)
+      arr[1]--;
+      arr[2]--
+      count++
+    }
+    return count
+};
