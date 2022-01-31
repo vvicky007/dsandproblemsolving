449f73b8cd5fa01fc8f4046777e7b7221b37d07e
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2022-01-31 19:38:52 +0530

    Time: 103 ms (44.13%), Space: 47.4 MB (5.22%) - LeetHub

--- 179-largest-number/179-largest-number.js
+++ 179-largest-number/179-largest-number.js
@@ -1,12 +1,3 @@
-const check = (obj)=>{
-  let flag = false
-  Object.keys(obj).forEach((x)=>{
-    if(obj[x].num.length>0){
-      flag = true
-    }
-  })
-  return flag
-}
 const compareString = (a='',b='')=>{
   let itr1 = 0,itr2=0,flag = 0, largest;
   if(a===b){
@@ -25,16 +16,10 @@ const compareString = (a='',b='')=>{
     itr2++;
   }
  
-    if(compareString(a+b,b+a)===a+b){
-      return a
-    }
-  else{
-    return b
-  }
+  return a
 }
 var largestNumber = function(nums) {
-
-   let flag = 0
+     let flag = 0
     nums = nums.map((x)=>x.toString())
     for(let i = 0 ; i < nums.length;i++){
       if(nums[i]!=='0'){
@@ -45,44 +30,18 @@ var largestNumber = function(nums) {
     if(flag ===0){
       return '0'
     }
+    nums = nums.sort((a,b)=>{
+      if(compareString(a+b,b+a)===a+b){
+        return -1
+      }
+      else if(compareString(a+b,b+a)===b){
+        return 1
+      }
+      return 0 
+    })
     let res = '',obj = {},max = '',maxidx,temp
     for(let i = 0 ; i < nums.length;i++){
-      if(obj[nums[i]]===undefined){
-        obj[nums[i]] = {num:nums[i],idx:i,occ:1}
-      }
-       else{
-        obj[nums[i]] = {num:nums[i],idx:i,occ:obj[nums[i]].occ+1}
-       }
+      res = res+nums[i]
     }
-    while(check(obj)){
-      max = '0'
-      Object.keys(obj).forEach((x,idx)=>{
-        if(obj[x].num&&obj[x].num.length>0){
-           if(parseInt(obj[x].num[0])===parseInt(max[0])){
-               
-              temp = compareString(max,obj[x].num)
-              max = temp
-              maxidx = obj[temp].idx
-          }
-          if(parseInt(obj[x].num[0])>parseInt(max[0])){
-            max = x
-            maxidx = obj[x].idx
-          }
-         
-        }
-        
-       
-      })
-      
-      for(let k = 0;k< obj[max].occ;k++){
-        res = res+max
-      }
-      obj[max] = {
-        num:'',
-        idx:obj[max].idx,
-        occ:0
-      }
-    }
-   
     return res
 };
