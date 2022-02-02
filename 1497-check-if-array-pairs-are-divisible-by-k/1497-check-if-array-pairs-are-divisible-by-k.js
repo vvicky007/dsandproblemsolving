85364ec0bf5e655091d5e06c3550f6d03592f60a
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2022-02-03 00:22:45 +0530

    Time: 195 ms (46.15%), Space: 61.9 MB (15.38%) - LeetHub

---
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {boolean}
+ */
+    
+    
+var canArrange = function(arr, k) {
+    
+    let map = new Map();
+    
+    for(let i of arr){
+        let currMod = i % k;
+        if(currMod < 0){
+            currMod += k;
+        } 
+        
+        if(map.has(currMod)) map.set(currMod,map.get(currMod) + 1);
+        else map.set(currMod, 1);
+    }
+    
+    if(map.get(0) % 2 == 1) return false;
+    
+    
+    for(let i = 1; i < k; i++){
+        if(map.get(i) !== map.get(k-i)) return false;
+    }
+    return true;
+    
+};
