4ada40c424f28c0bd0b4d1e5b7d517e5ba670847
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2022-02-03 21:14:23 +0530

    Time: 56 ms (98.68%), Space: 42.2 MB (11.84%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} palindrome
+ * @return {string}
+ */
+var breakPalindrome = function(palindrome) {
+     let lp = 0  ,res = 0,rp = palindrome.length-1
+  if(palindrome.length===1){
+    return ''
+  }
+  while(lp<palindrome.length){
+    if(palindrome[lp]!=='a'){
+      break
+    }
+    lp++;
+    rp--;
+  }
+  if(lp!==palindrome.length&&lp!==rp){
+    palindrome = palindrome.substring(0,lp)+'a'+palindrome.substring(lp+1,palindrome.length)
+  }
+  if(lp===palindrome.length||rp===lp){
+    palindrome = palindrome.substring(0,palindrome.length-1)+'b'
+  }
+ 
+ 
+  return palindrome
+};
