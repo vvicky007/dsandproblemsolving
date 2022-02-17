4b8ac0863dc0b14f60fc00fc9b5138342069231d
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2022-02-17 20:31:58 +0530

    Time: 84 ms (93.36%), Space: 51.7 MB (31.03%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,32 +3,12 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    if(prices.length===1){
-    return 0
+    let lowest = Infinity;
+  let max = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < lowest) lowest = prices[i];
+    max = Math.max(max, prices[i] - lowest);
   }
-    let min =[],max= [],res = Number.NEGATIVE_INFINITY
-    for(let i = 0 ; i < prices.length;i++){
-      if(i===0){
-        min[i] = prices[i]
-      }
-      else{
-        min[i] = Math.min(prices[i],min[i-1])
-      }
-    }
-    for(let i = prices.length-1 ; i >=0;i--){
-      if(i===prices.length-1){
-        min[i] = prices[i]
-      }
-      else{
-        max[i] = Math.max(prices[i],min[i+1])
-      }
-    }
-
-    for(let i = 0 ; i < prices.length;i++){
-      if(res<(max[i]-min[i])){
-        res = max[i]-min[i]
-      }
-    }
-    return res
+  return max;
     
 };
