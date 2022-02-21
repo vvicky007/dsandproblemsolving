07ac4d3bff8dad71f429c186109d28aa933e8aa7
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2022-02-21 17:45:05 +0530

    Time: 75 ms (95.11%), Space: 44.2 MB (10.68%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+   if(x===1){
+    return 1
+  }
+  let result ;
+  const bSearch = (s,e,target)=>{
+    if(s>e){
+      return 
+    }
+    let mid = Math.floor((s+e)/2)
+    if(mid*mid === target){
+      result = mid
+      return
+    }
+    else if(mid*mid>target){
+      return bSearch(s,mid-1,target)
+    }
+    else{
+      result = mid
+      return bSearch(mid+1,e,target)
+    }
+
+  }
+  bSearch(0,x,x)
+return result
+};
