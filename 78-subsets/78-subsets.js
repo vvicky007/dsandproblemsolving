1aab24cad4fb9d0e2cb9d5ca2844e8d8ad275d85
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2022-02-28 21:31:38 +0530

    Time: 98 ms (46.30%), Space: 48 MB (5.43%) - LeetHub

--- 78-subsets/78-subsets.js
+++ 78-subsets/78-subsets.js
@@ -3,19 +3,22 @@
  * @return {number[][]}
  */
 var subsets = function(nums) {
-       let subgenlimit = nums.length
-    subgenlimit = 2 << nums.length-1
-    let result = [], temp = [], bit
-    for(let i = 0 ; i < subgenlimit;i++){
-      temp = []
-      for(let j = 0 ; j < nums.length;j++){
-        let x =(1<<( nums.length-j-1))
-        bit = i & (1<<( nums.length-j-1))
-        if(bit>0){
-          temp = [...temp,nums[nums.length-j-1]]
-        }
-      }
-      result = [...result,temp]
+   let res = []
+  const generateSubSets = (nums = [],index,sub = [])=>{
+
+    if(index === nums.length){
+      return sub
     }
-    return result
+    let temp = generateSubSets(nums,index+1,[...sub,nums[index]])
+    if(temp){
+      res = [...res,temp] 
+    }
+    temp = generateSubSets(nums,index+1,sub)
+    if(temp){
+      res = [...res,generateSubSets(nums,index+1,sub)]
+    }
+    return null
+  }
+  generateSubSets(nums,0,[])
+  return res
 };
