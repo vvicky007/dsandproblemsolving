e2c7149f2eaf879f7d6c0ce59fa6502775b09f2f
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2022-03-19 12:58:22 +0530

    Time: 156 ms (76.60%), Space: 52.7 MB (70.48%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+      let  sortedMap = {},itr,res = []
+  for(let i = 0 ; i < strs.length;i++){
+    itr = strs[i].split('').sort((a,b)=>{
+      if(a>b){
+        return 1
+      }
+      else if(a<b){
+        return -1
+      }
+      return 0
+    })
+    itr = itr.join()
+    if(sortedMap[itr]!==undefined){
+      sortedMap[itr].push(strs[i])
+    }
+    else{
+      sortedMap[itr] = [strs[i]]
+    }
+
+  }
+  Object.keys(sortedMap).map((key)=>{
+    res.push(sortedMap[key])
+  })
+return res
+};
