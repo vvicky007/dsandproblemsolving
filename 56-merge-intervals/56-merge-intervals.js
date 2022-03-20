e174c871a13f8e6872fce86c971a6a45ec2c7f2a
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2022-03-20 17:30:28 +0530

    Time: 92 ms (96.84%), Space: 49.1 MB (55.05%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+ if(intervals.length<=1){
+    return intervals
+  }
+  let sortedIntervals = intervals.sort((a,b)=>{
+    if(a[0]>b[0]){
+      return 1
+    }
+    else if(a[0]<b[0]){
+      return -1
+    }
+    return 0
+  })
+  let cs = sortedIntervals[0][0],ce = sortedIntervals[0][1],itr = 1,count = 0, ci = 0,res = []
+  while(itr<sortedIntervals.length){
+    while(itr<sortedIntervals.length){
+      if((sortedIntervals[itr][1]>=cs&&sortedIntervals[itr][1]<=ce)||(sortedIntervals[itr][0]>=cs&&sortedIntervals[itr][0]<=ce)){
+        
+        if(ce<sortedIntervals[itr][1]){
+          ce = sortedIntervals[itr][1]
+        }
+      }
+      else{
+        break
+      }
+      itr++;
+    }
+    res.push([cs,ce])
+    if(itr>=sortedIntervals.length){
+      break
+    }
+    ce = sortedIntervals[itr][1]
+    cs = sortedIntervals[itr][0]
+  }
+  return res
+};
