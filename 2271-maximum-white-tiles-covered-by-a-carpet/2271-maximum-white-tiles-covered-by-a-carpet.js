d05f62731b9ee26cd3020c50055daf12588f891a
vvicky007 <36205785+vvicky007@users.noreply.github.com>
2022-05-15 07:31:50 +0530

    Time: 442 ms (100.00%), Space: 163.4 MB (100.00%) - LeetHub

---
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[][]} tiles
+ * @param {number} carpetLen
+ * @return {number}
+ */
+var maximumWhiteTiles = function(tiles, carpetLen) {
+   let max = 0;
+  tiles.forEach(data=>{
+  max = Math.max(max, data[0], data[1]);
+  });
+  if (carpetLen >= 434783959) {
+  let result = 0;
+  tiles.forEach(data=>{
+  result += (data[1]-data[0]+1);
+  });
+  return result;
+  }
+  let titleArray = new Array(max+1).fill(0);
+  tiles.forEach(data => {
+  for(let i = data[0]; i<=data[1];i++){
+  titleArray[i] = 1;
+  }
+  });
+  let tempMax = titleArray.slice(1, carpetLen+1).reduce((a, b) => a + b);
+  let result = tempMax;
+  for (let i = carpetLen+1; i<titleArray.length;i++) {
+  tempMax = tempMax - titleArray[i-carpetLen] + titleArray[i];
+  result = Math.max(result, tempMax);
+  }
+  return result;
+};
